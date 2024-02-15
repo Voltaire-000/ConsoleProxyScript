d1adda89437375b250076a3753fe20674643ac2b
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-15 19:07:59 +0700

    add code to import math

--- Program.cs
+++ Program.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ClearScript;
 
 namespace ConsoleProxyScript
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            var engine = new Microsoft.ClearScript.V8.V8ScriptEngine();
+
+            // add the math library
+            //engine.Execute("var math = require('math');");
+            engine.Execute("var Math = host.import('Math');");
+            Console.WriteLine("Hello World!");
         }
     }
 }
