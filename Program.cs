8b5ee0e90cf77f35baa8724648f5e0fe0a8524b0
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2024-02-15 20:05:01 +0700

    working console

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.ClearScript;
+using Microsoft.ClearScript.JavaScript;
 using Microsoft.ClearScript.V8;
 
 namespace ConsoleProxyScript
@@ -14,9 +15,26 @@ namespace ConsoleProxyScript
         static void Main(string[] args)
         {
             var engine = new Microsoft.ClearScript.V8.V8ScriptEngine();
-            engine.AddHostObject("host", new HostFunctions());
+            //engine.AddHostObject("host", new HostFunctions());
             //var math = engine.Evaluate("Math");
             var math = engine.Script.Math;
+
+            // expose a host type
+
+            engine.AddHostType("Console", typeof(Console));
+
+            engine.Execute("Console.WriteLine('{0} is an interesting number.', Math.PI)");
+            // expose a host object
+
+            engine.AddHostObject("random", new Random());
+
+            engine.Execute("Console.WriteLine(random.NextDouble())");
+            // expose entire assemblies
+
+            engine.AddHostObject("lib", new HostTypeCollection("mscorlib", "System.Core"));
+
+            engine.Execute("Console.WriteLine(lib.System.DateTime.Now)");
+
             var sinResult = math.sin(Math.PI / 6);
             var sqrtResult = math.sqrt(9);
 
